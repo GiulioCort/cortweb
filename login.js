35bc62db7222e21d2d498b026b447577d6ffb79f
GiulioCort <giulio.cortese09@gmail.com>
2025-03-01 23:14:24 +0100

    Add files via upload

---
+++ login.js
@@ -0,0 +1,19 @@
+let username;
+
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    if (username == "Ai0x") {
+        document.getElementById("1").style.display="block";}
+    else if (username == "hry6") {
+        document.getElementById("2").style.display="block";}
+    else if (username == "ioy3") {
+        document.getElementById("3").style.display="block";}
+    else if (username == "gt78") {
+        document.getElementById("4").style.display="block";}
+    else if (username == "rer2") {
+        document.getElementById("5").style.display="block";}
+    else if (username == "9dr4") {
+        document.getElementById("6").style.display="block";}
+    else if (username == "adgg765") {
+        document.getElementById("7").style.display="block";}
+}
